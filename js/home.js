fc20e9fab3ffed97e729dd3777ff733996a7cb09
AndresVargas0 <1458586@senati.pe>
2024-07-17 10:09:15 -0500

    Update(17/07/24)

--- js/home.js
+++ js/home.js
@@ -1,26 +1,47 @@
 // SLIDER
-document.addEventListener('DOMContentLoaded', function () {
-    const totalSlides = slides.length;
-    const slideWidth = slides[0].clientWidth;
-  
-    let currentIndex = 0;
-  
-    function goToSlide(index) {
-      if (index < 0) {
-        index = totalSlides - 1;
-      } else if (index >= totalSlides) {
-        index = 0;
-      }
-      currentIndex = index;
-      const offset = -currentIndex * slideWidth;
-      slider.style.transform = `translateX(${offset}px)`;
-    }
-  
-    setInterval(() => {
-      currentIndex++;
-      goToSlide(currentIndex);
-    }, 3000);
-  });
+const slider = document.getElementById("slider");
+let sliderSection = document.querySelectorAll(".slider_img");
+let sliderSectionLast = sliderSection[sliderSection.length -1];
+
+const btnRight = document.getElementById("btn_right");
+const btnLeft = document.getElementById("btn_left");
+
+function next() {
+    let sliderSectionFirst = document.querySelectorAll(".slider_img")[0];
+
+    slider.style.marginLeft = "-200%";
+    slider.style.transition = "margin-left 1s";
+    setTimeout(() => {
+        slider.style.transition = "none";
+        slider.insertAdjacentElement("beforeend", sliderSectionFirst)
+        slider.style.marginLeft = "-100%"
+    }, 1000);
+}
+
+function prev() {
+    let sliderSection = document.querySelectorAll(".slider_img");
+    let sliderSectionLast = sliderSection[sliderSection.length -1];
+    slider.style.marginLeft = "0";
+    slider.style.transition = "margin-left 1s";
+    setTimeout(() => {
+        slider.style.transition = "none";
+        slider.insertAdjacentElement("afterbegin", sliderSectionLast)
+        slider.style.marginLeft = "-100%"
+    }, 1000);
+
+}
+
+btnRight.addEventListener("click", function () {
+    next();
+})
+
+btnLeft.addEventListener("click", function () {
+    prev()
+})
+
+setInterval(() => {
+    next()
+}, 4000);
 
 // BARRA DE NAVEGACION
 fetch('/html/nav.html')
